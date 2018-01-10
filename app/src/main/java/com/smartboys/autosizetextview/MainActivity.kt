1ea325aabc705624dc0d0b0b0f2a28cea88cc487
Muhammad Maqsood <mmlilla90@gmail.com>
2018-01-10 13:14:18 +0500

    AutoSize textView

---
+++ app/src/main/java/com/smartboys/autosizetextview/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.smartboys.autosizetextview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    /*Three Types of auto sizing of textView
+
+    1:
+
+
+
+      */
+
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
